01d60e7bb1ed58379f95e3cc59810d1088574041
Martin Schicklgruber <martin.schicklgruber@solvistas.com>
2021-02-25 10:52:44 +0100

    Update for new Dataloader

--- test.py
+++ test.py
@@ -1,66 +1,42 @@
 import argparse
 import gc
 
-import cv2
-import dlib
 import torch
-import sys
-import os
-
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from networks.resnet import resnet50
-
-sys.path.append(os.getcwd())
-
-from src.util.data.data_loader import load_data
-from src.util.dotdict import Dotdict
+from src.data.basic_dataset import BasicDataset
+from src.data.transforms import create_basic_transforms
 from src.util.validate import calc_scores
 
 
-def get_opt():
-    opt = Dotdict()
-
-    opt.model = 'all'
-    opt.is_train = True
-    opt.pretrained = True
-    opt.checkpoints_dir = './out/checkpoints/faces'
-    opt.continue_train = True
-    opt.save_name = 'latest'
-    opt.name = 'knn'
-    # opt.dataset_path = './datasets/celeb-df-v2/faces'
-    opt.dataset_path = './datasets/forensic/faces_best'
-    opt.multiclass = False
-    opt.resize_interpolation = 'bilinear'
-    opt.load_size = -1
-    opt.train_split = 'train'
-    opt.train_size = 2500
-    opt.val_split = 'val'
-    opt.val_size = 100
-    opt.test_split = 'test'
-    opt.batch_size = 32
-
-    return opt
-
-
-def test(model_path, cuda):
+def test(args):
     gc.collect()
     torch.cuda.empty_cache()
-    print(cuda)
+    print(args.cuda)
     model = resnet50(num_classes=1)
-    if model_path is not None:
-        state_dict = torch.load(model_path, map_location='cpu')
+    state_dict = torch.load(args.model_path, map_location='cpu')
     model.load_state_dict(state_dict['model'])
     model.eval()
-    if cuda:
+    if args.cuda:
         model.cuda()
 
-    opt = get_opt()
-    test_loader = load_data(opt, opt.test_split)
+    normalize = {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]}
+
+    test_data = BasicDataset(root_dir=args.root_dir,
+                             processed_dir=args.processed_dir,
+                             crops_dir=args.crops_dir,
+                             split_csv=args.split_csv,
+                             seed=args.seed,
+                             normalize=normalize,
+                             transforms=create_basic_transforms(args.size),
+                             mode='test')
+    test_loader = DataLoader(test_data, batch_size=args.batch_size)
     y_pred, y_true = [], []
     for data, label in tqdm(test_loader):
         y_true.extend(label.flatten().tolist())
-        if cuda:
+        if args.cuda:
             data = data.cuda()
         pred = model(data).sigmoid().flatten().tolist()
         y_pred.extend(pred)
@@ -69,11 +45,22 @@ def test(model_path, cuda):
     print("Test: acc: {}; ap: {}; auc: {}".format(acc, ap, auc))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Parameters for Training")
+    args = parser.add_argument
+    # Dataset Options
+    args("--root_dir", default='datasets/dfdc', help="root directory")
+    args('--processed_dir', default='processed', help='directory where the processed files are stored')
+    args('--crops_dir', default='crops', help='directory of the crops')
+    args('--split_csv', default='folds.csv', help='Split CSV Filename')
+    args('--seed', default=111, help='Random Seed')
+    args('--size', default=255)
+
+    # Model Options
+    args('--model_path', '-mi', type=str, default='./src/baselines/cnn_detection/weights/blur_jpg_prob0.1.pth')
+    args('--cuda', type=bool, default=True)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('--model_path', '-mi', type=str,
-                   default='./src/baselines/cnn_detection/weights/blur_jpg_prob0.1.pth')
-    p.add_argument('--cuda', type=bool, default=True)
-    args = p.parse_args()
-    test(**vars(args))
+    test(**vars(parse_args()))
